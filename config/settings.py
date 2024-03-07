b3026ef63776cf3a322a844f7fcb7a9e483223d1
shaxboz01 <shaxbozaxrorov01@gmail.com>
2024-03-07 18:54:18 +0500

    fourth commit

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s7vfuzj7^jrrtesxf$_b%0f(dsfrzqd)3qivj0ou9_k3(j!1bv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
